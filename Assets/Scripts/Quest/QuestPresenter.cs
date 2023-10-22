a5d4a4a9cc125a830e1e209ffffa96efef734228
kawahara kazuma <kawahara.kazuma0525@gmail.com>
2023-10-22 15:54:05 +0900

    進行度の更新実装追加

--- Assets/Scripts/Quest/QuestPresenter.cs
+++ Assets/Scripts/Quest/QuestPresenter.cs
@@ -1,8 +1,24 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class QuestPresenter : MonoBehaviour
 {
+    [SerializeField] Text StageText;
+
+    int CurrenStage = 0;
+
+    void Start()
+    {
+        StageText.text = string.Format("Stage:{0}", CurrenStage);
+    }
+
+    public void OnNextButton()
+    {
+        CurrenStage++;
+        StageText.text = string.Format("Stage:{0}", CurrenStage);
+    }
+
     public void OnBackButton()
     {
         SceneManager.LoadScene("Town");
