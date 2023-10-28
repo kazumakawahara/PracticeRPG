f1770c88cc6c6dbb31f03faa6c899dc3763cc1be
kawahara kazuma <kawahara.kazuma0525@gmail.com>
2023-10-28 17:09:08 +0900

    playerModel作成

---
+++ Assets/Scripts/Quest/PlayerModel.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class PlayerModel : MonoBehaviour
+{
+    int maxHp;
+    int hp;
+    int at;
+    int currenStage;
+
+    public int MaxHP
+    {
+        get { return maxHp; }
+    }
+
+    public int HP
+    {
+        get { return hp; }
+    }
+
+    public int AT
+    {
+        get { return at; }
+    }
+
+    public int CurrenStage
+    {
+        get { return currenStage; }
+    }
+
+    public PlayerModel()
+    {
+        maxHp = 100;
+        hp = maxHp;
+        at = 6;
+        currenStage = 0;
+    }
+
+    public void NextStage()
+    {
+        currenStage++;
+    }
+
+    public void BackToTown()
+    {
+        currenStage = 0;
+    }
+}
